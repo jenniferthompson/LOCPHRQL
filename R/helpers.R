bb14a27387406d952a16a065bf52082c2ad5fb37
Jennifer Thompson <thompson.jennifer@gmail.com>
2018-10-23 17:20:36 -0500

    Add helper script

---
+++ R/helpers.R
@@ -0,0 +1,12 @@
+## -- Function to remove labels from variables to play nice with tidyverse -----
+clear.labels <- function(x) {
+  if(is.list(x)) {
+    for(i in 1 : length(x)) class(x[[i]]) <- setdiff(class(x[[i]]), 'labelled') 
+    for(i in 1 : length(x)) attr(x[[i]],"label") <- NULL
+  }
+  else {
+    class(x) <- setdiff(class(x), "labelled")
+    attr(x, "label") <- NULL
+  }
+  return(x)
+}
